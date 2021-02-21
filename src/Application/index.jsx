8d099756c1071afc0cbb312567ec3cdf44351a74
Lukas Gisder-Dubé <gisderdube@gmail.com>
2021-02-21 13:54:00 +0100

    setup

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -2,26 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 import GlobalStyles from "./GlobalStyles";
-import pic from "../assets/flower3-1600.jpg";
-
-const Heading = styled.h1`
-  color: green;
-`;
 
 const Application = () => {
   return (
     <>
       <GlobalStyles />
       <div>
-        <Heading>Hello from the Dubé React App Starter Template</Heading>
-        <p>
-          Donec sed odio dui. Morbi leo risus, porta ac consectetur ac,
-          vestibulum at eros. Donec sed odio dui. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Curabitur blandit tempus porttitor. Fusce
-          dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-          fermentum massa justo sit amet risus.
-        </p>
-        <img src={pic} alt="" style={{ maxWidth: "100%" }} />
+        <h1>React.js Message Queue (with Hooks) - Dubé Academy</h1>
       </div>
     </>
   );
