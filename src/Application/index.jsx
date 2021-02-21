0ea8d38b3ff4395b54faed0e8f987a5651c21af2
Lukas Gisder-Dubé <gisderdube@gmail.com>
2021-02-21 14:52:56 +0100

    finish up

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 import GlobalStyles from "./GlobalStyles";
 
+import MessageQueue, { useMessageQueue } from "./MessageQueue";
+
 const Application = () => {
+  const { addMessage, removeMessage, messages } = useMessageQueue();
+
   return (
     <>
       <GlobalStyles />
+      <MessageQueue messages={messages} removeMessage={removeMessage} />
       <div>
         <h1>React.js Message Queue (with Hooks) - Dubé Academy</h1>
+        <button
+          onClick={() => {
+            if (messages.length % 3 === 0) {
+              addMessage("Hello, this is an info message");
+            } else if (messages.length % 3 === 1) {
+              addMessage("Oh no :/", "error");
+            } else {
+              addMessage("Yayyy", "success");
+            }
+          }}
+        >
+          Add new Message
+        </button>
       </div>
     </>
   );
